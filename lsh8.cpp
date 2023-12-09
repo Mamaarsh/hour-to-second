bd29f5033b7deb3e520f0472b65d24262aee99cb
mamarsh <aj.silent.84@gmail.com>
2023-12-09 21:40:22 +0330

    embezzler

---
+++ lsh8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string a[n];
+    int b[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i] >> b[i];
+    }
+    for(int j=1;j<n;j++)
+    {
+        if(b[0]<b[j])
+        {
+            b[0] = b[j];
+            a[0] = a[j];
+        }
+    }
+    cout << a[0];
+
+    return 0;
+}
